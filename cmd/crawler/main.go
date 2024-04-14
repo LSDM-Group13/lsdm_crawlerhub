f1b94333d78f9534724239c2245edf87f4d119dd
cs <watchmaker3896@gmail.com>
2024-04-13 20:18:21 -0500

    add basic rest api functionality

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "github.com/LSDM-Group13/lsdm_crawlerhub/internal/crawler"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/LSDM-Group13/lsdm_crawlerhub/api"
+	"github.com/LSDM-Group13/lsdm_crawlerhub/internal/crawler"
+	"net/http"
+	url2 "net/url"
+	"strconv"
+)
+
+type Crawler struct {
+	hubBaseUrl     string
+	maxDomains     int
+	domainsToCrawl []string
+	domainsCrawled []string
+}
+
+func (c *Crawler) requestCrawlJobs(numDomains int) {
+	url, err := url2.Parse(c.hubBaseUrl + api.GetCrawlJobs.URL)
+	if err != nil {
+		fmt.Println("error forming url: ", err)
+		return
+	}
+	query := url.Query()
+	query.Set(api.GetCrawlJobs.Parameters.NumDomains, strconv.Itoa(numDomains))
+	url.RawQuery = query.Encode()
+	fmt.Println(url.String())
+
+	resp, err := http.Get(url.String())
+	if err != nil {
+		fmt.Println("error make request: ", err)
+	}
+
+	var crawlJobs api.CrawlJobs
+	err = json.NewDecoder(resp.Body).Decode(&crawlJobs)
+	if err != nil {
+		fmt.Println("error decoding response: ", err)
+	}
+
+	c.domainsToCrawl = crawlJobs.Domains
+	fmt.Println(c.domainsToCrawl)
+}
 
 func main() {
 	crawler.HelloCrawler()
+	c := Crawler{
+		hubBaseUrl:     "http://localhost:8869",
+		maxDomains:     3,
+		domainsToCrawl: nil,
+		domainsCrawled: nil,
+	}
+
+	c.requestCrawlJobs(2)
 }
--- cmd/hub/main.go
+++ cmd/hub/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "github.com/LSDM-Group13/lsdm_crawlerhub/internal/hub"
+import (
+	"fmt"
+	"github.com/LSDM-Group13/lsdm_crawlerhub/api"
+	"github.com/LSDM-Group13/lsdm_crawlerhub/internal/hub"
+	"github.com/gin-gonic/gin"
+	"math/rand/v2"
+	"net/http"
+	"strconv"
+)
+
+var domains = []string{"fakedomain1", "fakedomain2", "fakedomain3", "fakedomain4", "fakedomain5"}
+
+func selectDomains(numJobs int) []string {
+	var domainsSelected []string
+	for range numJobs {
+		domainsSelected = append(domainsSelected, domains[rand.IntN(len(domains))])
+	}
+	return domainsSelected
+}
+
+func getCrawlJobs(c *gin.Context) {
+	domainsRequested, exists := c.GetQuery("num_domains")
+	if !exists {
+		domainsRequested = "1"
+	}
+	numDomains, err := strconv.Atoi(domainsRequested)
+	if err != nil {
+		numDomains = 1
+	}
+	jobs := api.CrawlJobs{Domains: selectDomains(numDomains)}
+	c.IndentedJSON(http.StatusOK, jobs)
+}
 
 func main() {
 	hub.HelloHub()
+
+	router := gin.Default()
+	router.GET("/getCrawlJobs", getCrawlJobs)
+	err := router.Run("localhost:8869")
+	if err != nil {
+		fmt.Println("error running router: ", err)
+	}
 }
