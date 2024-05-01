aaa5ba03ad0772b18872950a2375b7e26309d0f5
cs <watchmaker3896@gmail.com>
2024-05-01 05:03:48 -0500

    add postCrawlData endpoint

--- cmd/hub/main.go
+++ cmd/hub/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/LSDM-Group13/lsdm_crawlerhub/api"
 	"github.com/LSDM-Group13/lsdm_crawlerhub/internal/hub"
 	"github.com/gin-gonic/gin"
+	"io"
 	"math/rand/v2"
 	"net/http"
 	"strconv"
@@ -33,11 +35,28 @@ func getCrawlJobs(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, jobs)
 }
 
+func postCrawlData(c *gin.Context) {
+	requestBody, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "failed to read request body"})
+		return
+	}
+
+	var domainData api.DomainData
+	if err := json.Unmarshal(requestBody, &domainData); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid request body"})
+		return
+	}
+
+	fmt.Println("Received domainData:", domainData)
+}
+
 func main() {
 	hub.HelloHub()
 
 	router := gin.Default()
 	router.GET("/getCrawlJobs", getCrawlJobs)
+	router.POST("/postCrawlData", postCrawlData)
 	err := router.Run("localhost:8869")
 	if err != nil {
 		fmt.Println("error running router: ", err)
