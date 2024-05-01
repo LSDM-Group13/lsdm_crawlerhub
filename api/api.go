54a62977dbb45a33c9998eed118d296e9cedae5e
cs <watchmaker3896@gmail.com>
2024-05-01 05:03:30 -0500

    add postCrawlData endpoint

--- api/api.go
+++ api/api.go
@@ -1,5 +1,19 @@
 package api
 
+type DomainData struct {
+	DomainName string
+	Pages      map[string]*string
+}
+
+func (dd DomainData) TotalSize() int {
+	totalSize := 0
+	for _, p := range dd.Pages {
+		totalSize += len(*p)
+	}
+
+	return totalSize
+}
+
 type CrawlJobs struct {
 	Domains []string `json:"domains"`
 }
@@ -14,6 +28,13 @@ type APIEndpoint struct {
 var (
 	GetCrawlJobs = APIEndpoint{
 		URL: "/getCrawlJobs",
+		Parameters: struct{ NumDomains string }{
+			NumDomains: "num_domains",
+		},
+	}
+
+	PostCrawlData = APIEndpoint{
+		URL: "/postCrawlData",
 		Parameters: struct{ NumDomains string }{
 			NumDomains: "num_domains",
 		},
