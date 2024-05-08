44db0aebe37d0d29f8010f002728c16a897b3f24
cs <watchmaker3896@gmail.com>
2024-05-08 01:27:01 -0500

    show one

--- scripts/image_search.py
+++ scripts/image_search.py
@@ -3,6 +3,7 @@ from PIL import Image
 from io import BytesIO
 import mysql.connector
 import sys
+import warnings
 
 connection = mysql.connector.connect(
     host="localhost",
@@ -17,8 +18,12 @@ if not connection.is_connected():
     print("Failed to connect to the database.")
     sys.exit(1)
 
-if len(sys.argv) > 2:
+if len(sys.argv) < 2:
     print("Enter a search term")
+
+show_one = False
+if len(sys.argv) > 2:
+    show_one = True
 
 search_term = sys.argv[1]
 
@@ -31,12 +36,24 @@ for (webpage_url, webpage_id, data) in cursor:
     print(f"{webpage_url}: {data}")
     query = "SELECT image_data FROM images WHERE webpage_id = %s ALLOW FILTERING"
     result = session.execute(query, (int(webpage_id),))
+
+    if show_one:
+        for i in range(2):
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", category=DeprecationWarning)
+                image_data = result[i].image_data
+            image = Image.open(BytesIO(image_data))
+            image.show()
+        break
+
     for row in result:
         image_data = row.image_data
         image = Image.open(BytesIO(image_data))
         image.show()
 
+
 session.shutdown()
+cursor.reset()
 cursor.close()
 connection.close()
 
